15b86c469a6702db84fdc74a7f6bf57c1db65731
Kedarinadh Sai Harsha <gaduharsha72@gmail.com>
2025-08-20 10:11:05 +0530

    succesfully completed

--- src/app/lib/firebase.js
+++ src/app/lib/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyCUzjWBK3gPnQ9FKtXi4tWEvmJeFGtmr2c",
-  authDomain: "my-nextjs-app-a1cea.firebaseapp.com",
-  projectId: "my-nextjs-app-a1cea",
-  storageBucket: "my-nextjs-app-a1cea.firebasestorage.app",
-  messagingSenderId: "1068328365235",
-  appId: "1:1068328365235:web:3b6170d1343149771bd4c6",
-  measurementId: "G-Y27K57C2Z5"
+  apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
